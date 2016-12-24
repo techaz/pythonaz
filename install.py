24644c3caba5c175700742b71d0ad0fe09311fd8
Rashad <techazweb@gmail.com>
2016-12-24 04:36:29 -0500

    Add files via upload

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import time,subprocess
 time.sleep(5)
-subprocess.call('python setup.py', shell=True)
+subprocess.call('./setup.sh', shell=True)
 subprocess.call('python run.py', shell=True)
